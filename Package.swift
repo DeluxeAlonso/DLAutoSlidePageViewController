957932cc2e8e43e3a13c4a4b6b9b4b8354b2c9cb
DeluxeAlonso <alonso.alvarez@pucp.pe>
2019-11-23 23:53:41 -0500

    Fix Package.swift source file path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DLAutoSlidePageViewController",
-            dependencies: []),
+            path: "Sources"
+        ),
         .testTarget(
             name: "DLAutoSlidePageViewControllerTests",
             dependencies: ["DLAutoSlidePageViewController"]),
