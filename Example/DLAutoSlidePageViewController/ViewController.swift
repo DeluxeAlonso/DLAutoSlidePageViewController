81f8c80425ca102df046158f67ea8983f46b3dc3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-04-07 00:24:48 -0500

    Updates example app

--- Example/DLAutoSlidePageViewController/ViewController.swift
+++ Example/DLAutoSlidePageViewController/ViewController.swift
@@ -33,7 +33,6 @@ class ViewController: UIViewController {
 
         // Configure default auto slide page configuration
         DefaultAutoSlideConfiguration.shared.timeInterval = 2.0
-        DefaultAutoSlideConfiguration.shared.hidePageControl = true
         let pageViewController = DLAutoSlidePageViewController(pages: pages)
         
         addChild(pageViewController)
