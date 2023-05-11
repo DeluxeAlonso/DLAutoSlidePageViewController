97bee598399056767fb6330326d42c95f2d8191b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-05-11 01:43:39 -0500

    Adds unit tests for presentation count

--- Tests/DLAutoSlidePageViewControllerTests.swift
+++ Tests/DLAutoSlidePageViewControllerTests.swift
@@ -9,31 +9,37 @@
 @testable import DLAutoSlidePageViewController
 import XCTest
 
+struct TestConfiguration: AutoSlideConfiguration {
+    var testHidesPageControl = false
+    var hidePageControl: Bool {
+        testHidesPageControl
+    }
+}
+
 final class DLAutoSlidePageViewControllerTests: XCTestCase {
 
     private var pageViewController: DLAutoSlidePageViewController!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testPresentationCount() {
+        // Arrange
+        let pages = [UIViewController(), UIViewController(), UIViewController()]
+        let pageViewController = DLAutoSlidePageViewController(pages: pages)
+        // Act
+        let presentationCount = pageViewController.presentationCount(for: pageViewController)
+        // Assert
+        XCTAssertEqual(presentationCount, pages.count)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPresentationZeroCountWhenHidesPageControl() {
+        // Arrange
+        var testConfiguration = TestConfiguration()
+        testConfiguration.testHidesPageControl = true
+        let pages = [UIViewController(), UIViewController(), UIViewController()]
+        let pageViewController = DLAutoSlidePageViewController(pages: pages, configuration: testConfiguration)
+        // Act
+        let presentationCount = pageViewController.presentationCount(for: pageViewController)
+        // Assert
+        XCTAssertEqual(presentationCount, 0)
     }
 
 }
